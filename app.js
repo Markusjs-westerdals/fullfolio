7fa55274b5025811086e9fc89cbcc856ae652247
Markus Sørem <sormar@westerdals.no>
2016-05-15 11:43:36 +0200

    My workking portofolio.

--- app.js
+++ app.js
@@ -1,24 +1,76 @@
+$(window).load(function() {
+  $('.loading').hide();
+});
+
+$('.project-details').scroll(function() {
+  var scrollTop = $(this).scrollTop();
+  $('.white-box').css('opacity', scrollTop / 100);
+  var rgbValue = scrollTop / 10;
+  //$('.navigation a').css('color', 'rgb(' + rgbValue + ',' + rgbValue + ',' + rgbValue +  ')');
+  console.log($(this).scrollTop());
+  if($(this).scrollTop() >= 100) {
+    // alert('the top');
+    $('.navigation a').css(
+      {
+      // 'background-color': 'white',
+      }
+    );
+  } else {
+    // $('.project-details>.navigation a').css(
+    //   {
+    //     'background-color': 'rgba(255, 255, 255, 0)'
+    //   });
+  }
+  //  if($(this).scrollTop() + $(this).height() == $(document).height()) {
+  //       alert('the top!');
+  //  }
+});
 $(document).ready(function() {
+  
+  // $('.white-box').css('opacity', 0.5);
     $('#fullpage').fullpage({
 
       // autoScrolling: false,
       scrollOverflow: true,
-      normalScrollElements: ".cv",
+      normalScrollElements: ".cv, .project-details",
       controlArrows: false,
+      // slidesNavigation: true,
+      // slidesNavPosition: 'top',
+      // easingcss3: "cubic-bezier(.10, .10, 0, .74)",
+      easingcss3: 'cubic-bezier(0.860, 0.000, 0.070, 1.000)',
       // showActiveTooltip: true,
-      navigation: true,
+      // navigation: true,
       anchors: ['hjem', 'dubi', 'kjottkutt', 'grafisk'],
       afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
-    }, onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+        if(anchorLink == "grafisk") {
+          loopBottom: true;
+        } else {
+          loopBottom: false;
+        }
+      },
+      onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
       if(anchorLink == 'hjem' && slideIndex == 0){
         $('.nav').show();
         $.fn.fullpage.setAllowScrolling(true, 'down');
-      };
-      if(anchorLink == 'hjem' && slideIndex == 1){
+      }
+      else if(anchorLink == 'hjem' && slideIndex == 1){
         $.fn.fullpage.setAllowScrolling(false, 'down');
+      };
+      if(anchorLink == 'dubi') {
+
+        if(slideIndex == 1) {
+          $.fn.fullpage.setAllowScrolling(true, 'down');
+          $.fn.fullpage.setAllowScrolling(true, 'up');
+        } else {
+          $.fn.fullpage.setAllowScrolling(false, 'down');
+          $.fn.fullpage.setAllowScrolling(false, 'up');
+        }
       }
     }
     });
+    $('.preview, .site-title, .navigation').on("click", function() {
+      $.fn.fullpage.moveSlideRight();
+    })
     $('.to-folio').on("click", function() {
       $.fn.fullpage.moveSectionDown();
     })
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,46 @@
-var gulp = require('gulp'),
-    jade = require('gulp-jade');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var concat = require('gulp-concat');
+var livereload = require('gulp-livereload');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
 
-// run this task by typing in gulp jade in CLI
-gulp.task('jade', function() {
-  return gulp.src('templates/**/*.jade')
-    .pipe(jade()) // pip to jade plugin
-    .pipe(gulp.dest('site')); // tell gulp our output folder
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({pretty: false}))
+        .pipe(gulp.dest('./css/'))
+        .pipe(livereload());
+});
+gulp.task('cv', function() {
+  gulp.src('sass/cv.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sass({pretty: false}))
+      .pipe(autoprefixer())
+      .pipe(cssmin())
+      .pipe(gulp.dest('./css/'))
+})
+
+gulp.task('views', function() {
+  gulp.src('index.pug')
+      .pipe(pug({pretty: false}))
+      .pipe(gulp.dest('.'))
+      .pipe(livereload());
+})
+
+//Watch task
+gulp.task('watch', function() {
+  gulp.watch('sass/**/*.scss',['styles']);
+  gulp.watch('sass/cv.scss',['cv']);
+  gulp.watch('templates/**/*.pug',['views']);
+});
+// gulp.task('default',function() {
+//     livereload.listen('8888')
+//
+// });
+
+gulp.task('default', ['watch', 'styles', 'cv', 'views'], function() {
+  livereload.listen('8888')
+  console.log('listening on 8888');
 });
